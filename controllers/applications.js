887f95f62aee9f80d078e52a70fc6c2f665649e5
GreyA <grey.altaer@gmail.com>
2025-02-20 09:51:46 -0800

    added POST route for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -20,5 +20,23 @@ router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
 });
 
+// POST /users/:userId/applications
+router.post('/', async (req, res) => {
+try {
+  // Extract
+  const currentUser = await User.findById(req.session.user._id);
+  // Transfer
+  currentUser.applications.push(req.body);
+  // Load
+  await currentUser.save();
+
+  res.redirect(`/users/${currentUser._id}/applications`);
+} catch (e) {
+    console.log(e);
+    res.redirect('/');
+}
+
+
+});
 
 module.exports = router;
